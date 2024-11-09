bd4990020596c442a6cec315cc7b41686aa736da
ayabarakat3 <ayabarakat14@gmail.com>
2024-11-09 17:36:31 -0500

    Create portfolio module with calculatePortfolioValue and getPortfolioAllocation functions.

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+// Create the Portfolio Module
+import { assets } from './asset.js';
+
+export function calculatePortfolioValue() {
+    return assets.reduce((total, asset) => total + asset.price * asset.quantity, 0);
+}
+// Function created to calculate total value of the portfolio
+export function getPortfolioAllocation() {
+    const totalSum = calculatePortfolioValue(); 
+    return assets.map(asset => ({
+    name: asset.name,
+    allocation: ((asset.price * asset.quantity) / totalSum) * 100
+    }));
+}
+// Function created to calculate the percentage allocation for every asset. 
